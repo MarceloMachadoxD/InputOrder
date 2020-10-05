e685035b1f5f70dceda55426aa1859abc8983b3a
Marcelo Machado <mastermanson@gmail.com>
2020-10-05 12:08:04 -0300

    Retirado get e set de OrderItem para ajuste de encapsulamento deixando possivel somente adicionar e remover itens pelos métodos removeItem() e addItem()

--- entities/Order.java
+++ entities/Order.java
@@ -61,14 +61,6 @@ public class Order {
 		this.client = client;
 	}
 
-	public List<OrderItem> getItems() {
-		return items;
-	}
-
-	public void setItems(List<OrderItem> items) {
-		this.items = items;
-	}
-
 
 	public void addItem(OrderItem item) {
 
